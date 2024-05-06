bc16a638200a0b156c88d91ec854a90c7f3ff279
HuntorGames <rt001a9138@gmail.com>
2024-05-06 18:27:56 +0100

    added UserProfileInformation model to get the profile information from the user upon registration and migrated

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfileInformation(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
+
+    display_name = models.CharField(max_length=50, null=True)
+    is_police = models.BooleanField(default=False)
+    profile_picture = models.ImageField(upload_to='profile_pics', blank=True)
+    
+    def __str__ (self):
+        return self.user.username
