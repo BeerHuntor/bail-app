25d7d4669bf2b5f68dcdec3d0f72ab48e5cfcbce
HuntorGames <rt001a9138@gmail.com>
2024-05-10 10:57:40 +0100

    sytled the form using widget attributes utilizing the BS5 class 'form-control'

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,5 +1,13 @@
 from django import forms
 
 class LoginModalForm(forms.Form):
-    username = forms.CharField(label="Username", max_length=30, required=True)
-    password = forms.CharField(widget=forms.PasswordInput, label="Password", required=True)
+    username = forms.CharField(label="Username", max_length=30, required=True,
+                               widget=forms.TextInput(attrs={
+                                   'placeholder' : 'Enter your username',
+                                   'class' : 'form-control'
+                               }))
+    password = forms.CharField(label="Password", required=True, 
+                               widget=forms.PasswordInput(attrs={
+                                   'placeholder' : 'Enter your password',
+                                   'class' : 'form-control'
+                               })) 
