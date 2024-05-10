7b43d4a379841f156ee2c1810167d8c29c111074
HuntorGames <rt001a9138@gmail.com>
2024-05-10 17:48:13 +0100

    created registration form based on modelforms.

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.models import User
+from authentication.models import UserProfileInformation
 
 class LoginModalForm(forms.Form):
     username = forms.CharField(label="Username", max_length=30, required=True,
@@ -11,3 +13,18 @@ class LoginModalForm(forms.Form):
                                    'placeholder' : 'Enter your password',
                                    'class' : 'form-control'
                                })) 
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(), max_length=25, required=True)
+
+    class Meta():
+        model = User
+        fields = ('username', 'password')
+
+class RegsisterModalForm(forms.ModelForm):
+    profile_picture = forms.ImageField(required=False)
+
+    class Meta():
+        model = UserProfileInformation
+        fields = ('profile_picture',)
+
