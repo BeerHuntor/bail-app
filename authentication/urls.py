b929a896e5e246cc3002ad00caf26d5a9c54b5b0
HuntorGames <rt001a9138@gmail.com>
2024-05-13 18:50:30 +0100

    added callback discord url

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from authentication.views import index_view, login_modal, discord_login, RegisterModalView
+from authentication.views import index_view, login_modal, discord_login, discord_callback, RegisterModalView
 
 app_name = 'authentication'
 urlpatterns = [
     path('', index_view, name="index"),
     path('login/', login_modal, name='login'),
     path('register/', RegisterModalView.as_view(), name='register'),
-    path('accounts/discord/login/', discord_login, name="discord_login"),
+    path('accounts/discord/login/', discord_login, name="discord_login"), # We bypass this via the below callback
+    path('accounts/discord/login/callback/', discord_callback, name="discord_callback"),
 ]
