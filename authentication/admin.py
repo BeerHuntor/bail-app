ba9863f75ee8a06f44e61ed6d8ea81fb7669b2f9
HuntorGames <rt001a9138@gmail.com>
2024-05-16 14:42:35 +0100

    added UserDiscordAccountInformation to admin

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from authentication.models import UserProfileInformation
+from authentication.models import UserProfileInformation, UserDiscordAccountInformation
 # Register your models here.
 admin.site.register(UserProfileInformation)
+admin.site.register(UserDiscordAccountInformation)
