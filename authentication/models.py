6559172c2003304c9ee0ed532dbd98bcea24f45e
HuntorGames <rt001a9138@gmail.com>
2024-05-16 13:19:10 +0100

    added DiscordAccountInformation model to store discord account information such as tokens and user_id and made migrations

--- authentication/models.py
+++ authentication/models.py
@@ -4,11 +4,16 @@ from django.contrib.auth.models import User
 # Create your models here.
 class UserProfileInformation(models.Model):
 
-    user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
 
-    display_name = models.CharField(max_length=50, null=True)
-    is_police = models.BooleanField(default=False)
     profile_picture = models.ImageField(upload_to='profile_pics', blank=True)
     
     def __str__ (self):
         return self.user.username
+    
+class UserDiscordAccountInformation(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    discord_user_id = models.CharField(max_length=255)
+    access_token = models.CharField(max_length=255)
+    refresh_token = models.CharField(max_length=255)
