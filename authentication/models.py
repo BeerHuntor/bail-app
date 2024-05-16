767ff21acb1fb6f704d2e8fb3a59cc2e05b03b46
HuntorGames <rt001a9138@gmail.com>
2024-05-16 14:42:08 +0100

    Made the datetime field of the UserDiscordAccountInformation model timezone aware to adjust for the correct timezone automatically

--- authentication/models.py
+++ authentication/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -14,7 +15,13 @@ class UserProfileInformation(models.Model):
 class UserDiscordAccountInformation(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    discord_user_id = models.CharField(max_length=255)
+    discord_user_id = models.CharField(max_length=255, null=True)
     access_token = models.CharField(max_length=255)
     refresh_token = models.CharField(max_length=255)
-    token_expiry = models.IntegerField(default=0)
+    token_expiry = models.DateTimeField(auto_now=False, auto_now_add=False)
+
+    def save(self,*args, **kwargs):
+        if not self.token_expiry.tzinfo:
+          # Make the token_expiry datetime aware using the current timezone
+            self.token_expiry = timezone.make_aware(self.token_expiry, timezone.get_current_timezone())
+        super().save(*args, **kwargs)
