49a51cbb75eee615c9f218607ac0c3c8225a195f
BeerHuntor <rt001a9138@gmail.com>
2024-05-20 22:59:46 +0100

    Added config file for easy access of all the users discord roles.

---
+++ authentication/roles_config.py
@@ -0,0 +1,20 @@
+ROLE_ID_TO_NAME = {
+    # RANKS
+    '1230652347307393087' : 'NEERP Police',
+    '1230652347290751030' : 'Probationary Police Constable',
+    '1230652347290751031' : 'Police Constable',
+    '1230652347290751032' : 'Sergeant',
+    '1231023367931297812' : 'Inspector',
+    '1230652347290751036' : 'Chief Inspector',
+    '1230652347298873505' : 'Chief Superintendant',
+    '1230652347298873506' : 'Assistant Chief Constable',
+    '1230652347298873511' : 'Deputy Chief Constable',
+    '1230652347298873512' : 'Chief Constable',
+    '1235956325884891146' : 'Bronze Command',
+    '1235956591338197074' : 'Silver Command',
+    '1235956707969073275' : 'Gold Command',
+
+    # OTHER
+    '1242231753813524572' : 'Training Officer',
+
+}
