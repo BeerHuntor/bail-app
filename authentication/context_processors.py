f8ed75bf2d00a10b5f11277b9048252659215b84
HuntorGames <rt001a9138@gmail.com>
2024-05-20 12:09:51 +0100

    adjusted context processors to use our custom user model

--- authentication/context_processors.py
+++ authentication/context_processors.py
@@ -1,10 +1,16 @@
-from authentication.models import UserProfileInformation
+from django.conf import settings
+from authentication.models import RegisteredUser
 
 # Context processors allow context to be sent with any request automatically,  so this instance is getting the UserProfielInformation model and passing it to every request to 
 # enable access for every template by default. 
+
 def user_profile(request):
     user_profile = None
     
     if request.user.is_authenticated:
-        user_profile = UserProfileInformation.objects.get_or_create(user=request.user)[0]
+        if isinstance(request.user, RegisteredUser):
+            try:
+                user_profile = RegisteredUser.objects.get(discord_user_id=request.user.discord_user_id)
+            except RegisteredUser.DoesNotExist:
+                user_profile = None
     return { 'user_profile' : user_profile }
