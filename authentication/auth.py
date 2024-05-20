696294d96f6cf7699c79ed1a6b2732c1e39287db
HuntorGames <rt001a9138@gmail.com>
2024-05-20 12:09:35 +0100

    Added custom authenticate method, which still doesn't get called.

---
+++ authentication/auth.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.backends import BaseBackend
+from authentication.models import RegisteredUser
+from django.contrib.auth.models import User
+
+class RegisteredUserAuthenticationBackend(BaseBackend):
+    def authenticate (self, request, user) -> RegisteredUser:
+        print("authenticate()")
+
+        if user is None:
+            return None
+
+        try: 
+            user_instance = RegisteredUser.objects.get(discord_user_id=user['id'])
+            return user_instance
+        except RegisteredUser.DoesNotExist:
+            print("User was not found... Saving!")
+
+            # Create a new user
+            
+        return None
+
+    def get_user(self, user_id):
+        try:
+            return RegisteredUser.objects.get(pk=user_id)
+        except RegisteredUser.DoesNotExist:
+            return None
