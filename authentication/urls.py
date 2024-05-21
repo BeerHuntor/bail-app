734a42ded46af14947fa746fee5f9138c57f8bdc
BeerHuntor <rt001a9138@gmail.com>
2024-05-21 17:12:53 +0100

    refactored urls and the callback urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from authentication.views import index_view, discord_login, discord_login_callback, discord_register, discord_register_callback
+from authentication.views import index_view, discord_login, discord_login_callback, discord_register, discord_register_callback, authenticate_user
 
 app_name = 'authentication'
 urlpatterns = [
     path('', index_view, name="index"),
-    path('login/', discord_login, name='login'),
-    path('login/callback/', discord_login_callback, name='discord_login_callback'),
-    path('register/', discord_register, name='register'),
-    path('register/callback/', discord_register_callback, name='discord_register_callback'),
+    path('login/', discord_login, name='discord_login'),
+    path('oauth2/login', discord_login_callback, name='discord_login_callback'),
+    path('register/', discord_register, name='discord_register'),
+    path('oauth2/register', discord_register_callback, name='discord_register_callback'),
+
 ]
 
