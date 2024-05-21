e5746a76c4b0132cd23362c75508081242946271
BeerHuntor <rt001a9138@gmail.com>
2024-05-21 17:26:41 +0100

    refactored auth.py to handle the new way of user_data being passed as a dictionary

--- authentication/auth.py
+++ authentication/auth.py
@@ -9,17 +9,19 @@ class RegisteredUserAuthenticationBackend(BaseBackend):
 
         try: 
             existing_user = RegisteredUser.objects.get(discord_user_id=user['id'])
-            print(f"Existing User: {existing_user}")
+            existing_user.access_token = user['access_token']
+            existing_user.token_expiry = user['token_expiry']
+            existing_user.refresh_token = user['refresh_token']
+
+            existing_user.save()
             return existing_user
+            
         except RegisteredUser.DoesNotExist:
             print("User was not found... Saving!")
 
             # Create a new user
             new_user = RegisteredUser.objects.create_user(user)
-            print(new_user)
-            return new_user
-            
-        
+            return new_user      
 
     def get_user(self, user_id):
         try:
